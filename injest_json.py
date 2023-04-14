9fab5939a79df9f8d55ce0756d0c50f7aeea19ff
jmoro0408 <james.moro.11@gmail.com>
2023-04-14 18:27:43 +0100

    move read_toml to utils

--- injest_json.py
+++ injest_json.py
@@ -8,25 +8,19 @@ from pathlib import Path
 from typing import Union
 
 import requests
-import tomli
+
+from utils import read_toml
 
 CONFIG_DIR = "credentials.toml"
-SAVE_FOLDER_DIR = Path(Path.cwd(), "json_files")
-
-
-def read_toml(toml_dir: Union[Path, str]) -> dict:
-    with open(toml_dir, mode="rb") as fp:
-        return tomli.load(fp)
-
 
 def request_json(url: str) -> dict:
     resp = requests.get(url=url)
     return resp.json()
 
 
-def save_json(json_file: dict) -> None:
+def save_json(json_file: dict, save_dir:Union[Path,str]) -> None:
     date_today = datetime.today().strftime("%Y-%m-%d")
-    jsonpath = Path(SAVE_FOLDER_DIR, f"{date_today}.json")
+    jsonpath = Path(save_dir, f"{date_today}.json")
     with open(jsonpath, "w") as f:
         json.dump(json_file, f, ensure_ascii=False)
 
@@ -34,5 +28,6 @@ def save_json(json_file: dict) -> None:
 if __name__ == "__main__":
     config = read_toml(CONFIG_DIR)
     url = config["parkrun_site"]["url"]
+    json_save_folder = config["json_directories"]["save_dir"]
     json_dict = request_json(url)
-    save_json(json_dict)
+    save_json(json_dict, json_save_folder)
