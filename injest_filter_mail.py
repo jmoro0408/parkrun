c44b7d73d7f048a5795350f214fbe23cc954e346
jmoro0408 <james.moro.11@gmail.com>
2023-04-17 22:01:30 +0100

    create main func

---
+++ injest_filter_mail.py
@@ -0,0 +1,9 @@
+from email_new_events import email_new_events_main
+from filter_uk_events import filter_uk_events_main
+from injest_json import injest_main
+
+
+def injest_filter_mail():
+    injest_main()
+    filter_uk_events_main()
+    email_new_events_main()
