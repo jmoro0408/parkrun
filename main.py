c66818c01cee5d506a89903e9cdd1949b09c5db7
jmoro0408 <james.moro.11@gmail.com>
2023-04-17 22:01:15 +0100

    removed

--- main.py
+++
@@ -1,8 +0,0 @@
-from email_new_events import email_new_events_main
-from filter_uk_events import filter_uk_events_main
-from injest_json import injest_main
-
-if __name__ == "__main__":
-    injest_main()
-    filter_uk_events_main()
-    email_new_events_main()
